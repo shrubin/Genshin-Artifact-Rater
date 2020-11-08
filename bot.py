ef92486ea0064af7258af0af5d1b047d0c63bb6c
Seth Rubin <seth.h.rubin@gmail.com>
2020-11-08 11:22:14 -0800

    change over to ocr space

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
-from rate_artifact import rate_artifact_char, rate_artifact_arti
+import rate_artifact as ra
 
 import os
 import requests
-from cv2 import cv2
 from discord.ext import commands
 from dotenv import load_dotenv
-import numpy as np
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+API_KEY = os.getenv('OCR_SPACE_API_KEY')
+
+ocr_url = 'https://api.ocr.space/parse/imageurl?apikey={0}&url={1}'
 
 bot = commands.Bot(command_prefix='-')
 
@@ -16,29 +17,24 @@ bot = commands.Bot(command_prefix='-')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-async def get_image(ctx):
-	try:
-		url = ctx.message.attachments[0].url
-		resp = requests.get(url, stream=True).raw
-		img = np.asarray(bytearray(resp.read()), dtype="uint8")
-		img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-		return img
-	except Exception as err:
-		print("Error: {0}".format(err))
-		await ctx.send('Error trying to fetch image')
+@bot.command(name='rate')
+async def rate(ctx):
+	url = ctx.message.attachments[0].url
+	resp = requests.get(ocr_url.format(API_KEY, url))
+	text = resp.json()['ParsedResults'][0]['ParsedText']
+	results = ra.parse(text)
+	score = ra.rate(results)
+	msg = 'Parsed Image: ' + str(results) + '\nGear Score: {0:.2f}%'.format(score * 100)
+	await ctx.send(msg)
 
 @bot.command(name='char')
 async def char(ctx):
-	img = await get_image(ctx)
-	final_pct, results = rate_artifact_char(img)
-	msg = 'Parsed Image: ' + str(results) + '\nGear Score: {0:.2f}%'.format(final_pct * 100)
+	msg = 'Deprecated, please use -rate'
 	await ctx.send(msg)
 
 @bot.command(name='arti')
 async def arti(ctx):
-	img = await get_image(ctx)
-	final_pct, results = rate_artifact_arti(img)
-	msg = 'Parsed Image: ' + str(results) + '\nGear Score: {0:.2f}%'.format(final_pct * 100)
+	msg = 'Deprecated, please use -rate'
 	await ctx.send(msg)
 
 bot.run(TOKEN)
--- rate_artifact.py
+++ rate_artifact.py
@@ -1,142 +1,90 @@
-from cv2 import cv2
-from fuzzywuzzy import fuzz, process
-import numpy as np
-import pytesseract
+import os
 import re
+import requests
+from dotenv import load_dotenv
+from fuzzywuzzy import fuzz, process
 
-custom_config = r'--oem 3 --psm 11'
+load_dotenv()
+API_KEY = os.getenv('OCR_SPACE_API_KEY')
 
-choices = ['HP', 'Healing', 'DEF', 'Recharge', 'Elemental', 'ATK', 'DMG', 'CRIT Rate']
+choices = ['HP', 'Healing Bonus', 'DEF', 'Energy Recharge', 'Elemental Mastery', 'ATK', 'CRIT DMG', 'CRIT Rate', 'Physical DMG Bonus']
 elements = ['Anemo', 'Electro', 'Pyro', 'Hydro', 'Cryo', 'Geo', 'Dendro']
-dmg_choices = elements + ['CRIT', 'Physical']
+choices += [element + ' DMG Bonus' for element in elements]
 
-reg = re.compile(r'(?:[IntS/\?\]\d]+,)?[IntS/\?\]\d]*\d[IntS/\?\]\d]*(?:,?\.,?[IntS/\?\]\d]+)?')
-backup = re.compile(r'[IntS/\?\]\d]*[IntS/\?\]]\.[IntS/\?\]\d]+')
+reg = re.compile(r'\d+(?:\.\d+)?')
+hp_reg = re.compile(r'\d,\d\d\d')
 
-max_mains = {'ATK': 311.0, 'ATK%': 46.6, 'Energy Recharge%': 51.8, 'Elemental': 187.0, 'Physical DMG%': 58.3,
-			 'CRIT Rate%': 31.1, 'CRIT DMG%': 62.2, 'Elem DMG%': 46.6}
-multiplier = 2
-max_subs = {'ATK': 19.0 * multiplier, 'Elemental': 23.0 * multiplier, 'Energy Recharge%': 6.5 * multiplier,
-			'ATK%': 5.8, 'CRIT Rate%': 3.9, 'CRIT DMG%': 7.8}
+max_mains = {'ATK': [311.0, 1], 'ATK%': [46.6, 1], 'Energy Recharge%': [51.8, 0.5], 'Elemental Mastery': [187.0, 0.5],
+			 'Physical DMG Bonus%': [58.3, 1], 'CRIT Rate%': [31.1, 1], 'CRIT DMG%': [62.2, 1], 'Elemental DMG Bonus%': [46.6, 1]}
+max_subs = {'ATK': [19.0, 0.5], 'Elemental Mastery': [23.0, 0.5], 'Energy Recharge%': [6.5, 0.5],
+			'ATK%': [5.8, 1], 'CRIT Rate%': [3.9, 1], 'CRIT DMG%': [7.8, 1]}
 
-def prepare_image(img):
-	out = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	out = cv2.threshold(out, 170, 255, cv2.THRESH_BINARY_INV)[1]
-	out = cv2.resize(out, (int(out.shape[1] * 3), int(out.shape[0] * 3)))
-	return out
-
-def parse_artifact(img, num_results):
-	text = pytesseract.image_to_string(img, config=custom_config)
+def parse(text):
 	# print(text)
-
-	cur_stat = None
-	count = 0
+	stat = None
 	results = []
 	for line in text.splitlines():
-		line = line.replace(' ','')
-		if line and len(line) > 1:
-			# print(line, fuzz.partial_ratio(line, 'Piece'))
-			if fuzz.partial_ratio(line, 'Piece') > 80 and len(line) > 3:
+		if not line:
+			continue
+		# print(line, fuzz.partial_ratio(line, 'Piece Set'))
+		if fuzz.partial_ratio(line, 'Piece Set') > 80 and len(line) > 4:
+			break
+		value = hp_reg.search(line)
+		if value:
+			print(line)
+			value = int(value[0].replace(',', ''))
+			results += [['HP', value]]
+			continue
+		extract = process.extractOne(line, choices, scorer=fuzz.partial_ratio)
+		# print(process.extract(line, choices, scorer=fuzz.partial_ratio))
+		if ((extract[1] > 80) and len(line) > 1) or stat:
+			print(line)
+			if (extract[1] > 80):
+				stat = extract[0]
+			value = reg.findall(line)
+			if not value:
+				continue
+			value = max(value, key=len).replace(',','')
+			if len(value) < 2:
+				continue
+			if value.isdigit():
+				value = int(value)
+			else:
+				value = float(value)
+				stat += '%'
+			results += [[stat, value]]
+			if len(results) == 5:
 				break
-			# print(line)
-			extract = process.extractOne(line, choices, scorer=fuzz.partial_ratio)
-			# print(process.extract(line, choices, scorer=fuzz.partial_ratio))
-			if ((extract[1] > 80) and len(line) > len(extract[0])-2) or count > 0:
-				# print(line)
-				if (extract[1] > 80):
-					count = 0
-					stat = extract[0]
-					if extract[0] == 'DMG':
-						extract_dmg = process.extractOne(line, dmg_choices, scorer=fuzz.partial_ratio)
-						if extract_dmg[1] <= 80:
-							continue
-						stat = extract_dmg[0] + ' DMG'
-				else:
-					stat = cur_stat
-				value = reg.findall(line)
-				if not value or len(max(value, key=len)) < 2:
-					value = backup.findall(line)
-					if not value or len(max(value, key=len)) < 2:
-						count -= 1
-						if count < 0:
-							cur_stat = stat
-							count = 2
-						continue
-				count = 0
-				value = max(value, key=len)
-				value = value.replace('I','1').replace('n','11').replace('t','1').replace('S','5').replace(
-									  '/','7').replace('?','7').replace(']','1')
-				num_digits = sum(v.isdigit() for v in value)
-				if num_digits > 3:
-					value = value.replace(',','').replace('.','')
-				elif value[-2] == ',':
-					value = value.replace(',','').replace('.','')
-					value = value[:-1] + '.' + value[-1]
-				else:
-					value = value.replace(',','')
-				if value.isdigit():
-					value = int(value)
-				else:
-					value = float(value)
-					stat += '%'
-				if any(stat == result[0] for result in results):
-					continue
-				results += [[stat, value]]
-				if len(results) == num_results:
-					break
 	return results
 
-def rate_artifact(results):
+def rate(results):
 	main = True
-	pct = 0.0
+	score = 0.0
 	total_weight = 16.5
 	main_weight = 8
-	half_weight = ['ATK', 'Elemental', 'Energy Recharge%']
 	for result in results:
 		print(result)
 		stat, value = result
 		if main:
 			main = False
-			key = stat if stat.split()[0] not in elements else 'Elem DMG%'
+			key = stat if stat.split()[0] not in elements else 'Elemental DMG Bonus%'
 			if key == 'HP':
 				total_weight -= main_weight
 			elif key in ['ATK%', 'CRIT Rate%', 'CRIT DMG%']:
 				total_weight -= 0.5
 			if key in max_mains:
-				pct += value / max_mains[key] * main_weight
+				score += value / max_mains[key][0] * max_mains[key][1] * main_weight
 		else:
 			if stat in max_subs:
-				pct += value / max_subs[stat]
-
-	final_pct = pct / total_weight
-	print(final_pct)
-	return final_pct
-
-def rate_artifact_char(img):
-	out = prepare_image(img)
-	cv2.imwrite('out.png', out)
-	results = parse_artifact(out, 5)
-	final_pct = rate_artifact(results)
-	return final_pct, results
-
-def rate_artifact_arti(img):
-	out = cv2.bitwise_not(prepare_image(img))
-
-	contours = cv2.findContours(out, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
-	c = max(contours, key = cv2.contourArea)
-	_,y,_,_ = cv2.boundingRect(c)
-
-	out1 = cv2.bitwise_not(out[:y,:])
-	cv2.imwrite('out1.png', out1)
-	results = parse_artifact(out1, 1)
-
-	out2 = out[y:,:]
-	cv2.imwrite('out2.png', out2)
-	results += parse_artifact(out2, 4)
-
-	final_pct = rate_artifact(results)
-	return final_pct, results
+				score += value / max_subs[stat][0] * max_subs[stat][1]
+	score = score / total_weight
+	print('Gear Score: {0:.2f}%'.format(score * 100))
+	return score
 
 if __name__ == '__main__':
-	img = cv2.imread('test.png')
-	rate_artifact_arti(img)
+	url = 'https://cdn.discordapp.com/attachments/774633095160397836/775014885012144138/image0.png'
+	ocr_url = 'https://api.ocr.space/parse/imageurl?apikey={0}&url={1}'
+	resp = requests.get(ocr_url.format(API_KEY, url))
+	text = resp.json()['ParsedResults'][0]['ParsedText']
+	results = parse(text)
+	score = rate(results)
