7550c931784d59c999841a7d36957f1220d7a289
Seth Rubin <seth.h.rubin@gmail.com>
2020-11-10 09:22:23 -0800

    fix validation

--- rate_artifact.py
+++ rate_artifact.py
@@ -17,9 +17,10 @@ reg = re.compile(r'\d+(?:\.\d+)?')
 hp_reg = re.compile(r'\d,\d{3}')
 
 max_mains = {'HP': [4780, 0], 'ATK': [311.0, 0.5], 'ATK%': [46.6, 1], 'Energy Recharge%': [51.8, 0.5], 'Elemental Mastery': [187.0, 0.5],
-			 'Physical DMG%': [58.3, 1], 'CRIT Rate%': [31.1, 1], 'CRIT DMG%': [62.2, 1], 'Elemental DMG%': [46.6, 1]}
-max_subs = {'ATK': [19.0, 0.5], 'Elemental Mastery': [23.0, 0.5], 'Energy Recharge%': [6.5, 0.5],
-			'ATK%': [5.8, 1], 'CRIT Rate%': [3.9, 1], 'CRIT DMG%': [7.8, 1]}
+			 'Physical DMG%': [58.3, 1], 'CRIT Rate%': [31.1, 1], 'CRIT DMG%': [62.2, 1], 'Elemental DMG%': [46.6, 1],
+			 'HP%': [46.6, 0], 'DEF%': [58.3, 0], 'Healing%': [35.9, 0]}
+max_subs = {'ATK': [19.0, 0.5], 'Elemental Mastery': [23.0, 0.5], 'Energy Recharge%': [6.5, 0.5], 'ATK%': [5.8, 1],
+			'CRIT Rate%': [3.9, 1], 'CRIT DMG%': [7.8, 1], 'DEF': [23.0, 0], 'HP': [299.0, 0], 'DEF%': [7.3, 0], 'HP%': [5.8, 0]}
 
 def ocr(url):
 	size = int(requests.get(url, stream=True).headers['Content-length'])
@@ -110,22 +111,22 @@ def rate(results):
 	total_weight = 16.5
 	main_weight = 8
 	for result in results:
-		print(result)
 		stat, value = result
 		if main:
 			main = False
 			key = stat if stat.split()[0] not in elements else 'Elemental DMG%'
 			if key in ['ATK%', 'CRIT Rate%', 'CRIT DMG%']:
 				total_weight -= 0.5
+			elif key in ['ATK', 'HP']:
+				total_weight -= main_weight * (1 - max_mains[key][1])
 			if key in max_mains:
 				value = validate(value, max_mains[key][0], '%' in stat)
 				score += value / max_mains[key][0] * max_mains[key][1] * main_weight
-				total_weight -= main_weight * (1 - max_mains[key][1])
-		else:
-			if stat in max_subs:
-				value = validate(value, max_subs[stat][0] * 6, '%' in stat)
-				score += value / max_subs[stat][0] * max_subs[stat][1]
+		elif stat in max_subs:
+			value = validate(value, max_subs[stat][0] * 6, '%' in stat)
+			score += value / max_subs[stat][0] * max_subs[stat][1]
 		result[1] = value
+		print(result)
 	score = score / total_weight
 	print('Gear Score: {0:.2f}%'.format(score * 100))
 	return score
